52a9e9a56f6bf752640b83a8d8bc33f59e9c163d
XPHEDRA <fati.gurciti1@gmail.com>
2019-05-19 20:07:51 +0300

    Doktor kan bağışı oluşturuldu ve eklendi

--- src/app/doktorblooddonors/doktorblooddonors.page.ts
+++ src/app/doktorblooddonors/doktorblooddonors.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController,NavParams } from '@ionic/angular';
+import { AngularFireDatabase ,AngularFireList} from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { kanBagisi } from 'src/models/Kan-Bagisi/kanbagis';
+import firebase from '@firebase/app';
+import { take } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-doktorblooddonors',
@@ -6,10 +13,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doktorblooddonors.page.scss'],
 })
 export class DoktorblooddonorsPage implements OnInit {
+  kanbagislistRef$: AngularFireList<kanBagisi>;
+  kanbagislistAsync$: any;
+  constructor(public actionSheetController: ActionSheetController, public dataBase: AngularFireDatabase) {
+     this.kanbagislistRef$ = this.dataBase.list('kanbagislist');
+     this.kanbagislistAsync$ = this.kanbagislistRef$.snapshotChanges();
+    }
+  
+    async selectkanbagis(kanbagisi: kanBagisi){
+  
+  
+      const actionSheet = await this.actionSheetController.create({
 
-  constructor() { }
-
-  ngOnInit() {
+        header: 'Menü',
+        buttons: [{
+          text: 'Sil',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => { this.kanbagislistRef$.remove((<any>kanbagisi).key);
+          //remove kısmı
+          }
+        }, {
+          text: 'İptal',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('İptal edildi');
+          }//iptal kısmı
+        }
+      ]
+      });
+      await actionSheet.present();
+  
+  
+    }
+  
+   ngOnInit() {
+   }
+  
   }
-
-}
