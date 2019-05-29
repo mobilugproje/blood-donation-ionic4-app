6f039c6e23620b4e77950946c7346416d8aedf1f
XPHEDRA <fati.gurciti1@gmail.com>
2019-05-29 17:04:06 +0300

    Kullanıcı şube görüntüle eklendi,db'ye bağlandı

--- src/app/subegoruntule/subegoruntule.page.ts
+++ src/app/subegoruntule/subegoruntule.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { subeBilgi } from '../../models/subeinfo';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-subegoruntule',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubegoruntulePage implements OnInit {
 
-  constructor() { }
+  subeBilgiRef$: Observable<any[]>;
 
+  constructor(public navCtrl: NavController, public dataBase: AngularFireDatabase) {
+    this.subeBilgiRef$ = this.dataBase.list('subebilgi').valueChanges();   
+  }
   ngOnInit() {
   }
 
 }
+
+
